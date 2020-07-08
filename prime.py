92e16507b460820556720dd384b671b87020589a
LambdaGaming <starwars18m@gmail.com>
2020-07-07 22:21:17 -0400

    Finished main Wheel of Fortune functionality

--- prime.py
+++ prime.py
@@ -2,6 +2,7 @@ import discord
 import random
 import json
 import requests
+import math
 
 BadWords = [ "communism", "china", "ussr", "stalin", "lenin", "putin", "vodka", "commie", "russia", "cuba", "vietnam", "mao", "castro", "bernie", "kim", "korea", "california", "red", "cyka", "blyat", "communist", "gulag", "chinese", "vietnamese", "korean", "californian", "reds", "communists", "gulags", "vodkas", "blizzard" ]
 
@@ -34,20 +35,21 @@ Quotes = [
 		will not work and he will not respond to any keywords.
 
 	Playing the game:
-		A single word will be selected from the random word generator and a player will have
-		to guess on that word. The player can get 5 letter guesses wrong before they lose a
-		point and can no longer guess on that word. Players can also choose to pass guessing
-		on a word to avoid losing points, but doing so will result in them losing a turn.
+		A single word will be selected from the random word generator and players will have
+		to guess on that word. Each player gets 5 letter guesses wrong before they lose a
+		point and can no longer guess on that word. Players can buy 3 more guesses at the cost
+		of 1 point. They can also guess what the word is at any time, but will lose a point if
+		they are wrong. For every 3 letters a player guesses correctly, they will receive a point.
 
 	Ending the game:
 		The same Discord users who are able to start the game can also end it using '!wof end'.
 		Once the game ends, a list of players and how many points they got will be printed out.
 		Players can then use these points to purchase server-related items such as weapons for
-		CityRP or becoming a class of their choice on SCP:SL.
+		CityRP or an item on SCP:SL.
 """
 
 def getRandomWord():
-	url = requests.get( "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json" ).json()
+	url = requests.get( "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json" ).json() # Pretty slow but it's good enough for now
 	nextWord = random.choice( list( url ) )
 	return nextWord
 
@@ -75,18 +77,26 @@ class WoF:
 	def nextWord( self ):
 		randomWord = getRandomWord()
 		self.setWord( randomWord )
+		self.setLetters( [] )
+		for ply in self.getPlayers():
+			ply.setTries( 5 )
 		return randomWord
 	
 	def getLetters( self ):
 		return self.letters
+
+	def setLetters( self, letters ):
+		self.letters = letters
 
 	def addLetter( self, letter ):
 		self.letters.append( letter.lower() )
 
 	def getFormattedWord( self, word ):
 		if not self.getLetters():
-			for letter in word:
-				word = word.replace( letter, "_" ) # FIXME: This prints 1-3 underscores no matter the length of the word, but outside of this environment it works correctly
+			underscore = ""
+			for i in range( len( word ) ):
+				underscore += "_"
+			return "`" + underscore + "`"
 		else:
 			letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 			for i in self.getLetters():
@@ -94,35 +104,66 @@ class WoF:
 			for i in letters:
 				if i in word:
 					word = word.replace( i, "_" )
-		return word
+		return "`" + word + "`"
+
+	def createPlayer( self, id ):
+		plyexists = False
+		for ply in self.getPlayers():
+			if ply.getID() == id:
+				plyexists = True
+				break
+		if not plyexists:
+			self.addPlayer( self.WoF_Player( id, 0, 5, 0 ) )
+
+	def getPlayerByID( self, id ):
+		FoundPlayer = None
+		for ply in self.getPlayers():
+			if ply.getID() == id:
+				FoundPlayer = ply
+		if FoundPlayer is None:
+			return None
+		else:
+			return FoundPlayer
 
 	class WoF_Player:
-		def __init__( self, id, points ):
+		def __init__( self, id, points, tries, correct ):
 			self.id = id
 			self.points = points
+			self.tries = tries
+			self.correct = correct
 		
 		def getID( self ):
 			return self.id
-		
-		def setID( self, id ):
-			self.id = id
 
 		def getPoints( self ):
 			return self.points
 		
-		def setPoints( self, points ):
-			self.points = points
+		def addPoints( self, points ):
+			self.points += points
 
-		@classmethod
-		def getAllPlayers( cls ):
-			players = set()
-			for ref in cls._instances:
-				ply = ref()
-				if ply is not None:
-					yield ply
-				else:
-					players.add( ply )
-			cls._instances -= ply
+		def removePoints( self, points ):
+			self.points -= max( min( self.points, math.inf ), 0 )
+
+		def getTries( self ):
+			return self.tries
+
+		def setTries( self, tries ):
+			self.tries = tries
+
+		def removeTry( self ):
+			self.tries -= max( min( 1, math.inf ), 0 )
+
+		def outOfTries( self ):
+			return self.tries <= 0
+
+		def addCorrect( self, correct ):
+			self.correct += correct
+
+		def setCorrect( self, correct ):
+			self.correct = correct
+
+		def getCorrect( self ):
+			return self.correct
 
 class MyClient( discord.Client ):
 	async def on_ready( self ):
@@ -136,63 +177,76 @@ class MyClient( discord.Client ):
 		if message.author.bot: return
 		split = message.content.split( " " )
 		lower = message.content.lower()
-		if message.content == "!wof test":
-			WoFGame = WoF( getRandomWord(), [], [] )
-			WoFGame.addLetter( "E" )
-			await message.channel.send( "Word format test: " + WoFGame.getFormattedWord( WoFGame.getWord() ) )
-			return
 		if WoFActive:
 			if split[0] == "!wof": #TODO: add check to external whitelist so only certain Discord IDs can use this command
-				try:
-					if split[1] == " ":
-						await message.channel.send( "List of available Wheel of Fortune commands: guessletter, guessword, end, nextword" )
-					elif split[1] == "guessletter":
-						try:
-							if split[2] == " " or not split[2].isalpha() or len( split[2] ) > 1:
-								await message.channel.send( "Please input a single letter for the guessletter command." )
-							else:
-								if not split[2] in WoFGame.getLetters():
-									await message.channel.send( "Guess letters so far: " + WoFGame.getFormattedWord() )
-								else:
-									await message.channel.send( "Letter '" + split[2].upper() + "' has already been guessed." )
-						except:
-							await message.channel.send( "Please input a single letter for the guessletter command." )
-					elif split[1] == "guessword":
-						try:
-							if split[2] == " " or not split[2].isalpha():
-								await message.channel.send( "Please input a word without numbers or special characters." )
-							else:
-								if split[2].lower() == WoFGame.getWord().lower():
-									await message.channel.send( message.author.name + " has guessed the correct word and has recived 1 point!" )
-									WoFGame.nextWord()
-									await message.channel.send( "Next word: " + WoFGame.getFormattedWord() )
-									#TODO: make a player instance if one doesn't already exist and give that player a point
-								else:
-									await message.channel.send( "The word you have guessed is incorrect." )
-						except:
-							await message.channel.send( "Please input a word without numbers or special characters." )
-					elif split[1] == "end":
-						del WoFGame
-						WoFActive = False
-						await message.channel.send( "Wheel of Fortune has ended. Returning to normal operations." )
-					elif split[1] == "nextword":
+				WoFGame.createPlayer( message.author.id )
+				MessagePlayer = WoFGame.getPlayerByID( message.author.id )
+				if 1 not in range( -len( split ), len( split ) ) or split[1] == " ":
+					await message.channel.send( "List of available Wheel of Fortune commands: guessletter, guessword, end, nextword, points, buyguesses" )
+				elif split[1] == "guessletter":
+					if 2 not in range( -len( split ), len( split ) ) or split[2] == " " or not split[2].isalpha() or len( split[2] ) > 1:
+						await message.channel.send( "Please input a single letter for the guessletter command." )
+						return
+					if split[2] in WoFGame.getLetters():
+						await message.channel.send( "Letter '" + split[2].upper() + "' has already been guessed." )
+						return
+					if MessagePlayer.outOfTries():
+						await message.channel.send( message.author.name + ", you are out of letter guesses!" )
+						return
+					if split[2] in WoFGame.getWord():
+						WoFGame.addLetter( split[2] )
+						MessagePlayer.addCorrect( 1 )
+						await message.channel.send( message.author.name + " has guessed a letter correctly!\nGuessed letters so far: " + WoFGame.getFormattedWord( WoFGame.getWord() ) )
+						if MessagePlayer.getCorrect() >= 3:
+							MessagePlayer.addPoints( 1 )
+							MessagePlayer.setCorrect( 0 )
+							await message.channel.send( message.author.name + " has guessed 3 correct letters and earned 1 point!" )
+					else:
+						MessagePlayer.removeTry()
+						await message.channel.send( "Letter '" + split[2].upper() + "' is not in this word. " + message.author.name + " has " + str( MessagePlayer.getTries() ) + " guesses left." )
+				elif split[1] == "guessword":
+					if 2 not in range( -len( split ), len( split ) ) or split[2] == " " or not split[2].isalpha():
+						await message.channel.send( "Please input a word without numbers or special characters." )
+						return
+					if split[2].lower() == WoFGame.getWord().lower():
+						await message.channel.send( message.author.name + " has guessed the correct word and received 1 point!" )
+						MessagePlayer.addPoints( 1 )
 						WoFGame.nextWord()
-						await message.channel.send( "Word has been forcibly changed to " + WoFGame.getFormattedWord() + "." )
-				except:
-					await message.channel.send( "List of available Wheel of Fortune commands: guessletter, guessword, end, nextword" )
+						await message.channel.send( "Next word: " + WoFGame.getFormattedWord( WoFGame.getWord() ) )
+					else:
+						MessagePlayer.removePoints( 1 )
+						await message.channel.send( message.author.name + " has incorrectly guessed the word and lost 1 point!" )
+				elif split[1] == "end":
+					del WoFGame
+					WoFActive = False
+					await message.channel.send( "Wheel of Fortune has ended. Returning to normal operations." )
+				elif split[1] == "nextword":
+					WoFGame.nextWord()
+					await message.channel.send( "Word has been forcibly changed to " + WoFGame.getFormattedWord( WoFGame.getWord() ) + "." )
+				elif split[1] == "points":
+					await message.channel.send( message.author.name + " currently has " + str( MessagePlayer.getPoints() ) + " point(s)." )
+				elif split[1] == "buyguesses":
+					if MessagePlayer.getTries() > 0:
+						await message.channel.send( message.author.name + ", you still have guesses remaining. You don't need to buy more." )
+						return
+					if MessagePlayer.getPoints() < 1:
+						await message.channel.send( message.author.name + ", you don't have enough points to buy more guesses." )
+						return
+					MessagePlayer.removePoints( 1 )
+					MessagePlayer.setTries( 3 )
+					await message.channel.send( message.author.name + " has purchased 3 more guesses for 1 point." )
 		else:
 			if split[0] == "!wof":
-				try:
-					if split[1] == " ":
-						await message.channel.send( "List of available Wheel of Fortune commands: start" )
-					elif split[1] == "start":
-						try:
-							WoFGame = WoF( getRandomWord(), [], [] )
-							await message.channel.send( "WHEEL OF FORTUNE MODE ACTIVATED" )
-						except:
-							await message.channel.send( "Something went wrong with the random word generator. Aborting." )
-				except:
+				if 1 not in range( -len( split ), len( split ) ) or split[1] == " ":
 					await message.channel.send( "List of available Wheel of Fortune commands: start" )
+					return
+				if split[1] == "start":
+					try:
+						WoFGame = WoF( getRandomWord(), [], [] )
+						await message.channel.send( "WHEEL OF FORTUNE MODE ACTIVATED\nWord: " + WoFGame.getFormattedWord( WoFGame.getWord() ) )
+					except:
+						await message.channel.send( "Something went wrong with the random word generator. GitHub might be down or the host of this bot might have a slow connection." )
+				return
 			if "hong kong" in lower:
 				await message.channel.send( "LIBERATE HONG KONG, REVOLUTION OF OUR AGE!" )
 				return
